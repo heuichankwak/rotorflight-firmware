4f6b73462e6f621a3ee55c6303b5bb74f53d9360
heuichankwak <40893019+heuichankwak@users.noreply.github.com>
2026-08-30 19:43:28 +0900

    123

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -15,7 +15,7 @@
  * along with this software. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <stdbool.h>
+#include <stdbool.h> 
 #include <stdint.h>
 #include <string.h>
 #include <math.h>
